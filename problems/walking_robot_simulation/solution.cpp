8956d024f0d972d0585d0a0e86d02b295d32639e
Ad603-tech <76997688+Ad603-tech@users.noreply.github.com>
2024-09-08 19:16:18 +0000

    Sync LeetCode submission - Walking Robot Simulation (cpp)

---
+++ problems/walking_robot_simulation/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        int max_d = 0, x = 0, y = 0;
+        pair<int, int> dir = {0, 1};
+        set<vector<int>>st(begin(obstacles), end(obstacles));
+        for(auto com:commands)
+        {
+            if(com == -1) dir = {dir.second, -dir.first};
+            else if(com == -2) dir = {-dir.second, dir.first};
+            else
+            {
+                for(int i = 0; i < com; i++)
+                {
+                    int newX = x + dir.first;
+                    int newY = y + dir.second;
+                    if(st.count({newX, newY})) break;
+                    x = newX, y = newY;
+                }
+            }
+            max_d = max(max_d, (x*x + y*y));
+        }
+        return max_d;
+    }
+};
