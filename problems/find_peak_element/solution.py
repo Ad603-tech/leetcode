04245bba0b6efde07ede686acaf271f002da958c
Ad603-tech <76997688+Ad603-tech@users.noreply.github.com>
2024-10-02 02:19:36 +0000

    Sync LeetCode submission - Find Peak Element (python3)

--- problems/find_peak_element/solution.py
+++ problems/find_peak_element/solution.py
@@ -1,5 +1,21 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        max_element = max(nums)
+        n = len(nums)
+        if n == 1:
+            return 0
+        if nums[n-1] > nums[n-2]:
+            return n-1
+        if nums[0] > nums[1]:
+            return 0
+
+        low, high = 0, n - 2
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1]:
+                return mid
+            elif nums[mid] < nums[mid+1]:
+                low = mid + 1
+            else:
+                high = mid - 1
         
-        return nums.index(max_element)
+        return -1
