8b3dcce206c336a8ef177849f1eb14df90fef2e1
Ad603-tech <76997688+Ad603-tech@users.noreply.github.com>
2024-11-24 22:11:07 +0000

    Sync LeetCode submission - Find First and Last Position of Element in Sorted Array (python3)

--- problems/find_first_and_last_position_of_element_in_sorted_array/solution.py
+++ problems/find_first_and_last_position_of_element_in_sorted_array/solution.py
@@ -1,42 +1,42 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def findfirst(nums, target):
+        def first_occ(nums, target):
+            n = len(nums)
             low = 0
-            high = len(nums) - 1
-            first_pos = -1
-
-            while low <= high:
-                mid = (low + high) // 2
-
-                if nums[mid] == target:
-                    first_pos = mid
-                    high = mid - 1
-
-                elif nums[mid] < target:
-                    low = mid + 1
-            
-                else:
-                    high = mid - 1
-            return first_pos
-
-        def findlast(nums, target):
-            low = 0
-            high = len(nums) - 1
-            last_pos = -1
+            high = n - 1
+            fo = -1
 
             while low <= high:
                 mid = (low + high) // 2
                 if nums[mid] == target:
-                    last_pos = mid
+                    fo = mid
+                    high = mid - 1
+                elif target >= nums[mid]:
                     low = mid + 1
-                elif nums[mid] < target:
-                    low = mid + 1
-            
                 else:
                     high = mid - 1
-            return last_pos
+                
+            return fo
+        
+        def last_occ(nums, target):
+            n = len(nums)
+            low = 0
+            high = n - 1
+            lo = -1
 
-        first_occ = findfirst(nums, target)
-        last_occ = findlast(nums, target)
+            while low <= high:
+                mid = (low + high) // 2
+                if nums[mid] == target:
+                    lo = mid
+                    low = mid + 1
+                elif target <= nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            return lo
+        
+        fo = first_occ(nums, target)
+        lo = last_occ(nums, target)
+        return [fo, lo]
 
-        return [first_occ, last_occ]
+                
