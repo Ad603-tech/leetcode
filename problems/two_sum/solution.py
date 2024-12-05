7c4dd22e59b3c943147d43bea91670167741d6b9
Ad603-tech <76997688+Ad603-tech@users.noreply.github.com>
2024-12-05 01:50:19 +0000

    Sync LeetCode submission - Two Sum (python3)

--- problems/two_sum/solution.py
+++ problems/two_sum/solution.py
@@ -1,9 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
-        for i in range(n - 1):
-            for j in range(i + 1, n):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        num_index = {}
+        for index, num in enumerate(nums):
+            complement = target - num
+
+            if complement in num_index:
+                return [num_index[complement], index]
+            
+            num_index[num] = index
         
         return []
