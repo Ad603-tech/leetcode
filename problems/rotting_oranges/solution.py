b1e4b2d895236be7d57063c8a096ee1f5bde2b3e
Ad603-tech <76997688+Ad603-tech@users.noreply.github.com>
2024-12-07 01:45:55 +0000

    Sync LeetCode submission - Rotting Oranges (python3)

---
+++ problems/rotting_oranges/solution.py
@@ -0,0 +1,30 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        queue = deque()
+        fresh = 0
+        time = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    fresh += 1
+                elif grid[r][c] == 2:
+                    queue.append((r, c))
+        
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        while queue and fresh > 0:
+            for _ in range(len(queue)):
+                x, y = queue.popleft()
+                for dx, dy in directions:
+                    nx, ny = x + dx, y + dy
+                    if 0 <= nx < rows and 0 <= ny < cols and grid[nx][ny] == 1:
+                        grid[nx][ny] = 2
+                        fresh -= 1
+                        queue.append((nx, ny))
+            time += 1
+
+        if fresh == 0:
+            return time
+        else:
+            return -1
