e5cec99c960d6c3001d3c68e6f860195ae6b9047
Ad603-tech <76997688+Ad603-tech@users.noreply.github.com>
2025-02-01 00:23:44 +0000

    Sync LeetCode submission - Sort Colors (python3)

--- problems/sort_colors/solution.py
+++ problems/sort_colors/solution.py
@@ -3,19 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        low, mid, high = 0, 0, len(nums) - 1
-
-        while mid <= high:
+        left = 0
+        mid = 0
+        right = len(nums) - 1
+        while mid <= right:
             if nums[mid] == 0:
-                nums[low], nums[mid] = nums[mid], nums[low]
-                low += 1
+                nums[left], nums[mid] = nums[mid], nums[left]
+                left += 1
                 mid += 1
             elif nums[mid] == 1:
                 mid += 1
-            
             else:
-                nums[mid], nums[high] = nums[high], nums[mid]
-                high -= 1
-            
-        
+                nums[mid], nums[right] = nums[right], nums[mid]
+                right -= 1
         return nums
