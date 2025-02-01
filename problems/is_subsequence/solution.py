d9c84e1c342952b8e5e75a818b1e26030c23c89f
Ad603-tech <76997688+Ad603-tech@users.noreply.github.com>
2025-02-01 00:45:49 +0000

    Sync LeetCode submission - Is Subsequence (python3)

--- problems/is_subsequence/solution.py
+++ problems/is_subsequence/solution.py
@@ -6,9 +6,6 @@ class Solution:
         while i < len(s) and j < len(t):
             if s[i] == t[j]:
                 i += 1
-            
             j += 1
-
+        
         return i == len(s)
-
-
