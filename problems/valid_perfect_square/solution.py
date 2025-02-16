c8e3493bc3293c75d22a0105f789033eeb161af5
Ad603-tech <76997688+Ad603-tech@users.noreply.github.com>
2025-02-16 19:20:41 +0000

    Sync LeetCode submission - Valid Perfect Square (python3)

---
+++ problems/valid_perfect_square/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        left = 0
+        right = num
+
+        while left <= right:
+            m = (left + right) // 2
+            if m * m == num:
+                return True
+            elif m * m > num:
+                right = m - 1
+            else:
+                left = m + 1
+        
+        return False
